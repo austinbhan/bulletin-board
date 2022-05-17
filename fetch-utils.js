6e0fb33e7fcfee6db38f92378159b6b5711f508b
austinbhan <austinbhan@outlook.com>
2022-05-17 12:19:07 -0700

    Functioning fetchPosts() function

---
+++ fetch-utils.js
@@ -0,0 +1,14 @@
+const SUPABASE_URL = 'https://ekvctprnmhlihrtxfomc.supabase.co';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVrdmN0cHJubWhsaWhydHhmb21jIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTIzMTI1MDcsImV4cCI6MTk2Nzg4ODUwN30.3DwFaO1PRQPJQBj3OEs8ed-pA5qQ_7kbxTiw3YzzplY';
+
+const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+
+export async function fetchPosts() {
+    const response = await client.from('POST_HISTORY').select('*');
+
+    return response.data;
+}
+
+export async function loadData() {
+    
+}
