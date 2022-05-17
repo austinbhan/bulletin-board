878e9f42a6d692eab149386e4b45e1953f05046a
austinbhan <austinbhan@outlook.com>
2022-05-17 13:01:08 -0700

    Made utils.js folder

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 // import functions and grab DOM elements
-import { loadData } from './fetch-utils.js';
+import { fetchPosts } from './fetch-utils.js';
 
+async function loadData() {
+    const posts = await fetchPosts();
+    console.log(posts);
+    const main = document.getElementById('show-posts');
 
+// Need to Create For Loop
+}
 
+loadData();
 // let state
 
 // set event listeners 
---
+++ utils.js
@@ -0,0 +1,4 @@
+export function renderPost(post) {
+    const div = document.createElement('div');
+    div.setAttribute('id', post.title);
+}
