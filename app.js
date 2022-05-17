9ea045c8a5600def2f02f5c2dcdcd6d558f07ef2
austinbhan <austinbhan@outlook.com>
2022-05-17 16:49:54 -0700

    Supabase array shows in Front End

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 // import functions and grab DOM elements
 import { fetchPosts } from './fetch-utils.js';
+import { renderPost } from './utils.js';
 
 async function loadData() {
     const posts = await fetchPosts();
     console.log(posts);
     const main = document.getElementById('show-posts');
 
-// Need to Create For Loop
+    for (let post of posts) {
+        const postDiv = renderPost(post);
+        main.append(postDiv);
+    }
 }
 
 loadData();
--- utils.js
+++ utils.js
@@ -1,4 +1,16 @@
 export function renderPost(post) {
     const div = document.createElement('div');
-    div.setAttribute('id', post.title);
+    div.setAttribute('class', 'post-container');
+
+    const h4 = document.createElement('h4');
+    h4.textContent = `${post.title}`;
+
+    const h5 = document.createElement('h5');
+    h5.textContent = `${post.user}`;
+
+    const p = document.createElement('p');
+    p.textContent = `${post.post}`;
+
+    div.append(h4, h5, p);
+    return div;
 }
