6ba7fc6283ec35738c6a98cabf6859154174069e
austinbhan <austinbhan@outlook.com>
2022-05-17 17:58:25 -0700

    Created Sign Up Form and creation-page

--- login-screen/login-screen.js
+++ login-screen/login-screen.js
@@ -1,9 +1,14 @@
-`import { signUpUser } from '../fetch-utils.js';`
+import { signUpUser } from '../fetch-utils.js';
 
 const signUpForm = document.getElementById('sign-up-form');
 
 signUpForm.addEventListener('submit', (e) => {
     e.preventDefault();
     const data = new FormData(signUpForm);
-    console.log({ email: data.get('email'), password: data.get('password') }); //Entries Showing Null?
+    console.log({ email: data.get('new-email'), password: data.get('new-password') });
+
+    const user = await signUpUser(data.get('new-email'), data.get('new-password'))
+    if (user) {
+        location.replace('../creation-page') // Placeholder
+    }
 });
