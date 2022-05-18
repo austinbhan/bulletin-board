840d1d426b4a650169850c4b63c3797a4667f72c
austinbhan <austinbhan@outlook.com>
2022-05-17 18:02:25 -0700

    Functioning Sign Up and Supabase Log

--- login-screen/login-screen.js
+++ login-screen/login-screen.js
@@ -2,13 +2,13 @@ import { signUpUser } from '../fetch-utils.js';
 
 const signUpForm = document.getElementById('sign-up-form');
 
-signUpForm.addEventListener('submit', (e) => {
+signUpForm.addEventListener('submit', async (e) => {
     e.preventDefault();
     const data = new FormData(signUpForm);
     console.log({ email: data.get('new-email'), password: data.get('new-password') });
 
-    const user = await signUpUser(data.get('new-email'), data.get('new-password'))
+    const user = await signUpUser(data.get('new-email'), data.get('new-password')); 
     if (user) {
-        location.replace('../creation-page') // Placeholder
+        location.replace('../creation-page'); // Placeholder, Functioning Sign Up Button
     }
 });
